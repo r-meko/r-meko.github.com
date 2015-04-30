2185246c8305a4e2b2a8b2267db5f20f88e8fe18
Raymond Pizarro <rp745@macterm02.falabs.rutgers.edu>
2015-04-29 20:55:28 -0400

    nav buttons

---
+++ assignment2/files/js/utilities.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+	$('.project').magnificPopup({
+		type: 'image'
+	});
+
+	$('.gallery').magnificPopup({
+		type: 'image',
+		gallery:{
+			enabled: true
+		}
+	});
+});
